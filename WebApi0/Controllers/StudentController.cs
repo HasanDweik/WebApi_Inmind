3761924d2ff734b39598319454ae7877c3bb3f45
Hasan Dweik <201720576@ua.edu.lb>
2022-06-27 12:56:51 +0300

    first commit

---
+++ WebApi0/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi0.Models;
+
+namespace WebApi0.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentController : Controller
+{
+    private List<Student> students = new List<Student>{new Student(1,"Hasan","hasan7@live.com"),
+        new Student(2,"George","george3@live.com")};
+    
+    private readonly ILogger<StudentController> _logger;
+
+    public StudentController(ILogger<StudentController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetStudent")]
+    public IEnumerable<Student> Get()
+    {
+        return students
+            .ToArray();
+    }
+    [HttpPost(Name = "PostStudent")]
+    public IEnumerable<Student> AddStudent(Student student)
+    {
+        students.Add(student);
+        return Get();
+    }
+    [HttpDelete(Name = "DeleteStudent")]
+    public IEnumerable<Student> DeleteStudent(int id)
+    {
+        Student student = students.Where(obj => obj.id == id).Select(x => x).First();
+        students.Remove(student);
+        return Get();
+    }
+    
+}
---
+++ WebApi0/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi0.Models;
+
+public class Student
+{
+    
+    public Student(int id, string name, string email)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    [Required]
+    public int id { get; set; }
+
+    [Required]
+    [MinLength(3,ErrorMessage = "please enter a valid name")]
+
+    public string name { get; set; }
+    [Required]
+    public string email { get; set; }
+
+}
